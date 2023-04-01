7293e7fb24c2b8cb04be319c6754522211ac8dd0
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-04-01 01:47:48 -0300

    styles home

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card } from "@mui/material";
 import { Web3Button } from "@web3modal/react";
 
 export default function Home() {
@@ -10,13 +10,68 @@ export default function Home() {
 					justifyContent: "center",
 					flexDirection: "column",
 					alignItems: "center",
+					marginTop: 16,
 				}}
 			>
-				<Typography>Keywise</Typography>
-				<Typography>Sarasa</Typography>
-				<Box>
+				<Typography
+					variant="h1"
+					sx={{
+						fontSize: {
+							lg: 140,
+							md: 90,
+							sm: 70,
+							xs: 50,
+						},
+						fontWeight: 700,
+						marginBottom: 4,
+					}}
+				>
+					Keywise
+				</Typography>
+				<Typography
+					sx={{
+						fontSize: {
+							lg: 30,
+							md: 28,
+							sm: 24,
+							xs: 20,
+						},
+						maxWidth: {
+							lg: 600,
+							md: 500,
+							sm: 400,
+							xs: 300,
+						},
+						fontWeight: 400,
+						textAlign: "center",
+						marginBottom: 4,
+					}}
+				>
+					Transformamos los contratos tradicionales de alquiler en contratos
+					inteligentes.
+				</Typography>
+				<Card
+					sx={{
+						minWidth: {
+							lg: 600,
+							md: 400,
+							sm: 350,
+							xs: 300,
+						},
+						minHeight: {
+							lg: 300,
+							md: 300,
+							sm: 250,
+							xs: 200,
+						},
+						backgroundColor: "#9FE870",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
 					<Web3Button />
-				</Box>
+				</Card>
 			</Box>
 		</>
 	);
