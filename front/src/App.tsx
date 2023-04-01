1fd1a6d39c352f4d5d4df6ce80350416bbd6d2df
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-04-01 14:24:13 -0300

    changes in style home

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,6 +14,7 @@ const chains = [arbitrum, mainnet, polygon];
 const projectId = import.meta.env.VITE_PROJECT_ID;
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+
 const wagmiClient = createClient({
 	autoConnect: true,
 	connectors: w3mConnectors({ projectId, version: 1, chains }),
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,78 +1,87 @@
 import { Box, Typography, Card } from "@mui/material";
 import { Web3Button } from "@web3modal/react";
+import city from "./../assets/city.png";
 
 export default function Home() {
 	return (
 		<>
-			<Box
-				sx={{
-					display: "flex",
-					justifyContent: "center",
-					flexDirection: "column",
-					alignItems: "center",
-					marginTop: 16,
-				}}
-			>
-				<Typography
-					variant="h1"
+			<img src={city} style={{ position: "absolute", bottom: "0" }}></img>
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<Box
 					sx={{
-						fontSize: {
-							lg: 140,
-							md: 90,
-							sm: 70,
-							xs: 50,
-						},
-						fontWeight: 700,
-						marginBottom: 4,
-					}}
-				>
-					Keywise
-				</Typography>
-				<Typography
-					sx={{
-						fontSize: {
-							lg: 30,
-							md: 28,
-							sm: 24,
-							xs: 20,
-						},
-						maxWidth: {
-							lg: 600,
-							md: 500,
-							sm: 400,
-							xs: 300,
-						},
-						fontWeight: 400,
-						textAlign: "center",
-						marginBottom: 4,
-					}}
-				>
-					Transformamos los contratos tradicionales de alquiler en contratos
-					inteligentes.
-				</Typography>
-				<Card
-					sx={{
-						minWidth: {
-							lg: 600,
-							md: 400,
-							sm: 350,
-							xs: 300,
-						},
-						minHeight: {
-							lg: 300,
-							md: 300,
-							sm: 250,
-							xs: 200,
-						},
-						backgroundColor: "#9FE870",
+						alignItems: "center",
+						marginTop: 16,
+						position: "fixed",
 						display: "flex",
 						justifyContent: "center",
-						alignItems: "center",
+						flexDirection: "column",
+						bottom: 0,
 					}}
 				>
-					<Web3Button />
-				</Card>
-			</Box>
+					<Typography
+						variant="h1"
+						sx={{
+							fontSize: {
+								lg: 140,
+								md: 90,
+								sm: 70,
+								xs: 50,
+							},
+							fontWeight: 700,
+							marginBottom: 4,
+							fontFamily: "'Spectral'",
+							color: "#265700",
+						}}
+					>
+						Keywise
+					</Typography>
+					<Typography
+						sx={{
+							fontSize: {
+								lg: 30,
+								md: 28,
+								sm: 24,
+								xs: 20,
+							},
+							maxWidth: {
+								lg: 600,
+								md: 500,
+								sm: 400,
+								xs: 300,
+							},
+							fontWeight: 400,
+							textAlign: "center",
+							marginBottom: 4,
+							fontFamily: "'lato'",
+						}}
+					>
+						Transformamos los contratos tradicionales de alquiler en contratos
+						inteligentes.
+					</Typography>
+					<Card
+						sx={{
+							minWidth: {
+								lg: 600,
+								md: 400,
+								sm: 350,
+								xs: 300,
+							},
+							minHeight: {
+								lg: 300,
+								md: 300,
+								sm: 250,
+								xs: 200,
+							},
+							backgroundColor: "#9FE870",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Web3Button />
+					</Card>
+				</Box>
+			</div>
 		</>
 	);
 }
