b42f26095d0c5a7b5c59d1c6b3987a4abb7abcbb
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2023-04-02 12:58:59 -0300

    chages styles in form

--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -1,10 +1,10 @@
 import { Box, Typography, Card, TextField, Button, Grid } from "@mui/material";
-import InputAdornment from '@mui/material/InputAdornment';
+import InputAdornment from "@mui/material/InputAdornment";
 import { useFormik, useField } from "formik";
 import { object, number, string, ObjectSchema } from "yup";
 import { User } from "../typings/index";
 
-const validationSchema: ObjectSchema<User> = object( {
+const validationSchema: ObjectSchema<User> = object({
 	fullName: string(),
 	email: string(),
 	phone: number(),
@@ -21,11 +21,11 @@ const validationSchema: ObjectSchema<User> = object( {
 	city: string(),
 	state: string(),
 	zipCode: number(),
-} );
+});
 
 //REEMPLAZAR TODA LA INFO QUE VIENE DE WALLET DE HASH Y DE CONTRATO POR LOS DATOS DE ESTADO PARA MANDAR EL FORM
 export default function Register() {
-	const formik = useFormik( {
+	const formik = useFormik({
 		initialValues: {
 			fullName: "fullName",
 			email: "email@example.com",
@@ -45,35 +45,80 @@ export default function Register() {
 			zipCode: 1022,
 		},
 		validationSchema: validationSchema,
-		onSubmit: ( values ) => {
-			alert( JSON.stringify( values, null, 2 ) );
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
 		},
-	} );
+	});
 
 	// Convert in base 44 our file
-	const convertBase44 = ( files: any ) => {
-		new Promise( ( resolve, reject ) => {
+	const convertBase44 = (files: any) => {
+		new Promise((resolve, reject) => {
 			const fileReader = new FileReader();
-			console.log( files[0], "hhh" );
-			fileReader.readAsDataURL( files![0] );
+			console.log(files[0], "hhh");
+			fileReader.readAsDataURL(files![0]);
 			fileReader.onload = () => {
-				const base44 = ( fileReader.result as string ).substring(
-					( fileReader.result as string ).indexOf( "," ) + 1
+				const base44 = (fileReader.result as string).substring(
+					(fileReader.result as string).indexOf(",") + 1
 				);
-				console.log( base44 );
+				console.log(base44);
 			};
-		} );
+		});
 	};
 
 	return (
 		<>
 			<form onSubmit={formik.handleSubmit}>
-				<Typography>Subir contrato</Typography>
-				<Typography>
-					Lorem ipsum ddolor sit amet, consectetur adipiscing elit, sed do
-					eiusmodtempor incididunt ut labore et dolore magna aliqua.
-				</Typography>
-				<Typography>Datos del Usuario</Typography>
+				<Box
+					sx={{
+						width: "80%",
+						display: "flex",
+						justifyContent: "center",
+						flexDirection: "column",
+						marginLeft: "8em",
+					}}
+				>
+					<Typography
+						sx={{
+							fontSize: {
+								lg: 30,
+								md: 28,
+								sm: 24,
+								xs: 20,
+							},
+							maxWidth: {
+								lg: 600,
+								md: 500,
+								sm: 400,
+								xs: 300,
+							},
+							fontWeight: 500,
+							textAlign: "left",
+							marginBottom: 4,
+							marginTop: 4,
+							fontFamily: "'lato'",
+						}}
+					>
+						Subir contrato
+					</Typography>
+					<Typography
+						sx={{
+							fontSize: {
+								lg: 24,
+								md: 22,
+								sm: 18,
+								xs: 16,
+							},
+							fontWeight: 400,
+							textAlign: "left",
+							marginBottom: 4,
+							fontFamily: "'lato'",
+						}}
+					>
+						Lorem ipsum ddolor sit amet, consectetur adipiscing elit, sed do
+						eiusmodtempor incididunt ut labore et dolore magna aliqua.
+					</Typography>
+					<Typography>Datos del Usuario</Typography>
+				</Box>
 				<Box
 					sx={{
 						flexGrow: 1,
@@ -97,9 +142,26 @@ export default function Register() {
 								type="text"
 								value={formik.values.fullName}
 								onChange={formik.handleChange}
-								error={formik.touched.fullName && Boolean( formik.errors.fullName )}
+								error={
+									formik.touched.fullName && Boolean(formik.errors.fullName)
+								}
 								helperText={formik.touched.fullName && formik.errors.fullName}
 								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
 							/>
 						</Grid>
 						<Grid item xs={5}>
@@ -107,109 +169,291 @@ export default function Register() {
 								placeholder="DNI"
 								id="dni"
 								name="dni"
-								type='number'
+								type="number"
 								value={formik.values.dni}
 								onChange={formik.handleChange}
-								error={formik.touched.dni && Boolean( formik.errors.dni )}
+								error={formik.touched.dni && Boolean(formik.errors.dni)}
 								helperText={formik.touched.dni && formik.errors.dni}
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
 							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Phone"
+							<TextField
+								placeholder="Phone"
 								id="phone"
 								name="phone"
-								type='number'
+								type="number"
 								value={formik.values.phone}
 								onChange={formik.handleChange}
-								error={formik.touched.phone && Boolean( formik.errors.phone )}
-								helperText={formik.touched.phone && formik.errors.phone} fullWidth />
+								error={formik.touched.phone && Boolean(formik.errors.phone)}
+								helperText={formik.touched.phone && formik.errors.phone}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Email"
+							<TextField
+								placeholder="Email"
 								id="email"
 								name="email"
-								type='email'
+								type="email"
 								value={formik.values.email}
 								onChange={formik.handleChange}
-								error={formik.touched.email && Boolean( formik.errors.email )}
-								helperText={formik.touched.email && formik.errors.email} fullWidth />
+								error={formik.touched.email && Boolean(formik.errors.email)}
+								helperText={formik.touched.email && formik.errors.email}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Propietario o Inquilino"
+							<TextField
+								placeholder="Propietario o Inquilino"
 								id="status"
 								name="status"
-
 								value={formik.values.status}
 								onChange={formik.handleChange}
-								error={formik.touched.status && Boolean( formik.errors.status )}
-								helperText={formik.touched.status && formik.errors.status} fullWidth />
+								error={formik.touched.status && Boolean(formik.errors.status)}
+								helperText={formik.touched.status && formik.errors.status}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="wallet"
+							<TextField
+								placeholder="wallet"
 								id="account"
 								name="account"
-
 								value={formik.values.account}
 								onChange={formik.handleChange}
-								error={formik.touched.account && Boolean( formik.errors.account )}
+								error={formik.touched.account && Boolean(formik.errors.account)}
 								helperText={formik.touched.account && formik.errors.account}
-								fullWidth />
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={10}>
 							<Typography>Dirección</Typography>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Street Name"
+							<TextField
+								placeholder="Street Name"
 								id="streetName"
 								name="streetName"
-
 								value={formik.values.streetName}
 								onChange={formik.handleChange}
-								error={formik.touched.streetName && Boolean( formik.errors.streetName )}
-								helperText={formik.touched.streetName && formik.errors.streetName} fullWidth />
+								error={
+									formik.touched.streetName && Boolean(formik.errors.streetName)
+								}
+								helperText={
+									formik.touched.streetName && formik.errors.streetName
+								}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Street Number"
+							<TextField
+								placeholder="Street Number"
 								id="streetNumber"
 								name="streetNumber"
 								type="number"
 								InputProps={{
-									startAdornment: <InputAdornment position="start">#</InputAdornment>,
+									startAdornment: (
+										<InputAdornment position="start">#</InputAdornment>
+									),
 								}}
 								value={formik.values.streetNumber}
 								onChange={formik.handleChange}
-								error={formik.touched.streetNumber && Boolean( formik.errors.streetNumber )}
-								helperText={formik.touched.streetNumber && formik.errors.streetNumber} fullWidth />
+								error={
+									formik.touched.streetNumber &&
+									Boolean(formik.errors.streetNumber)
+								}
+								helperText={
+									formik.touched.streetNumber && formik.errors.streetNumber
+								}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="City"
+							<TextField
+								placeholder="City"
 								id="city"
 								name="city"
-
 								value={formik.values.city}
 								onChange={formik.handleChange}
-								error={formik.touched.city && Boolean( formik.errors.city )}
-								helperText={formik.touched.city && formik.errors.city} fullWidth />
+								error={formik.touched.city && Boolean(formik.errors.city)}
+								helperText={formik.touched.city && formik.errors.city}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="State"
+							<TextField
+								placeholder="State"
 								id="state"
 								name="state"
-
 								value={formik.values.state}
 								onChange={formik.handleChange}
-								error={formik.touched.state && Boolean( formik.errors.state )}
-								helperText={formik.touched.state && formik.errors.state} fullWidth />
+								error={formik.touched.state && Boolean(formik.errors.state)}
+								helperText={formik.touched.state && formik.errors.state}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="ZIP code"
+							<TextField
+								placeholder="ZIP code"
 								id="zipCode"
 								name="zipCode"
 								type="number"
 								value={formik.values.zipCode}
 								onChange={formik.handleChange}
-								error={formik.touched.zipCode && Boolean( formik.errors.zipCode )}
-								helperText={formik.touched.zipCode && formik.errors.zipCode} fullWidth />
+								error={formik.touched.zipCode && Boolean(formik.errors.zipCode)}
+								helperText={formik.touched.zipCode && formik.errors.zipCode}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}></Grid>
 
@@ -217,42 +461,104 @@ export default function Register() {
 							<Typography>Contrato</Typography>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Contract type"
+							<TextField
+								placeholder="Contract type"
 								id="contractName"
 								name="contractName"
-
 								value={formik.values.contractName}
 								onChange={formik.handleChange}
-								error={formik.touched.contractName && Boolean( formik.errors.contractName )}
-								helperText={formik.touched.contractName && formik.errors.contractName}
-								fullWidth />
+								error={
+									formik.touched.contractName &&
+									Boolean(formik.errors.contractName)
+								}
+								helperText={
+									formik.touched.contractName && formik.errors.contractName
+								}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Deposit"
+							<TextField
+								placeholder="Deposit"
 								id="deposit"
 								name="deposit"
 								type="number"
 								InputProps={{
-									startAdornment: <InputAdornment position="start">$</InputAdornment>,
+									startAdornment: (
+										<InputAdornment position="start">$</InputAdornment>
+									),
 								}}
 								value={formik.values.deposit}
 								onChange={formik.handleChange}
-								error={formik.touched.deposit && Boolean( formik.errors.deposit )}
-								helperText={formik.touched.deposit && formik.errors.deposit} fullWidth />
+								error={formik.touched.deposit && Boolean(formik.errors.deposit)}
+								helperText={formik.touched.deposit && formik.errors.deposit}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
 						<Grid item xs={5}>
-							<TextField placeholder="Rent"
+							<TextField
+								placeholder="Rent"
 								id="rent"
 								name="rent"
 								type="number"
 								InputProps={{
-									startAdornment: <InputAdornment position="start">$</InputAdornment>,
+									startAdornment: (
+										<InputAdornment position="start">$</InputAdornment>
+									),
 								}}
 								value={formik.values.rent}
 								onChange={formik.handleChange}
-								error={formik.touched.rent && Boolean( formik.errors.rent )}
-								helperText={formik.touched.rent && formik.errors.rent} fullWidth />
+								error={formik.touched.rent && Boolean(formik.errors.rent)}
+								helperText={formik.touched.rent && formik.errors.rent}
+								fullWidth
+								sx={{
+									"& .MuiOutlinedInput-root": {
+										borderRadius: "40px",
+										backgroundColor: "#ECFAE2",
+										"& fieldset": {
+											borderColor: "transparent",
+										},
+										"&:hover fieldset": {
+											borderColor: "#265700",
+										},
+										"&.Mui-focused fieldset": {
+											borderColor: "#265700",
+										},
+									},
+								}}
+							/>
 						</Grid>
+						<Grid item xs={5}></Grid>
 						<Grid item xs={10}>
 							<Button
 								sx={{
@@ -265,13 +571,27 @@ export default function Register() {
 									type="file"
 									id="files"
 									style={{ visibility: "hidden" }}
-									onChange={( e ) => convertBase44( e.target.files )}
+									onChange={(e) => convertBase44(e.target.files)}
 								/>
 							</Button>
 						</Grid>
 						<Grid item xs={10}>
-							<Button variant="contained">Cancelar</Button>
-							<Button variant="contained" type="submit">
+							<Button
+								variant="contained"
+								sx={{
+									backgroundColor: "#265700",
+									marginRight: "6em",
+								}}
+							>
+								Cancelar
+							</Button>
+							<Button
+								variant="contained"
+								type="submit"
+								sx={{
+									backgroundColor: "#265700",
+								}}
+							>
 								Siguiente
 							</Button>
 						</Grid>
